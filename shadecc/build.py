63494c77941da3837f5b743dc2028c624ecdec76
Jacob Milligan <jacobpmilligan@gmail.com>
2018-04-06 23:12:10 +1000

    feat/GLSL: flatten UBO's for GLSL output

--- shadecc/build.py
+++ shadecc/build.py
@@ -12,26 +12,65 @@ from shadecc.template import generate_cpp
 
 
 def get_src_name(shader):
+    """
+    Gets a formatted filename for a shader object
+    :param shader:
+    :return:
+    """
     return shader.name + '.' + shader.stage + '.glsl'
 
 
+def compile_glsl(dir, spv, shader_obj):
+    """
+    Compiles GLSL from SPIR-V code and flattens all uniform blocks
+    :param dir: The shaders parent build directory
+    :param spv: SPIR-V file name
+    :param shader_obj:
+    """
+    output_path = shader_obj.name + '.' + shader_obj.stage + '.glsl'
+    cmd = [bin.get_path('spirv-cross'), '--version', '330', spv, '--output', output_path,
+           '--flatten-ubo']
+    output = subprocess.check_output(cmd, cwd=dir)
+    if len(output) > 0:
+        tqdm.write(bytes.decode(output).strip())
+    with open(os.path.join(dir, output_path), 'r') as glsl_file:
+        shader_obj.glsl_src = glsl_file.read()
+
+
 def compile_spirv(dir, shader):
+    """
+    Compiles preprocessed GLSL code to SPIR-V
+    :param dir: Location to output temp file and .spv
+    :param shader:
+    :return:
+    """
+    # write to temp file
+    temp_path = os.path.join(dir, get_src_name(shader))
+    with open(temp_path, 'w') as src:
+        src.write(shader.glsl_src)
+
     spv_name = get_src_name(shader).replace('glsl', 'spv')
     src_name = get_src_name(shader)
     tool = bin.get_path('glslangValidator')
     cmd = [tool, '--aml', '-S', shader.stage, '-G', '-o', spv_name, src_name]
     out = subprocess.check_output(cmd, cwd=dir)
     tqdm.write(bytes.decode(out).strip())
+
+    os.remove(temp_path)
     return spv_name
 
 
-def dump_src(dir, shader):
-    path = os.path.join(dir, get_src_name(shader))
-    with open(path, 'w') as src:
-        src.write(shader.glsl_src)
-
-
 def build(input_paths, output_dir, pattern, recurse):
+    """
+    Builds all shaders in the specified input paths, outputting the result into the specified
+    output directory
+    :param input_paths: list of paths to search for glsl source files
+    :param output_dir: directory to output compiled shaders - maintains the corresponding input
+    paths folder structure
+    :param pattern: The file extension pattern used by all shaders (*.glsl by default)
+    :param recurse: If true, will search recursively in all child directories of each input path
+    :return:
+    """
     preprocessor = Preprocessor()
     for in_path in input_paths:
         glob_pattern = os.path.join(in_path, '**') if recurse else in_path
@@ -50,13 +89,18 @@ def build(input_paths, output_dir, pattern, recurse):
             path = os.path.join(output_dir, location)
             if not os.path.exists(path):
                 os.mkdir(path)
-            dump_src(path, shader_obj)
 
             bar.desc = 'Compiling SPIR-V'
             spv = compile_spirv(path, shader_obj)
+
+            bar.desc = 'Compiling GLSL'
+            compile_glsl(path, spv, shader_obj)
+
             bar.desc = 'Compiling MSL'
             metal.compile(path, spv, shader_obj)
+
             bar.desc = 'Writing C++ files'
             generate_cpp(output_dir, shader_obj)
+
             bar.desc = 'Done'
 
