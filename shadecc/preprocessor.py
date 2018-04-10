47136c5dbdc3759b252780a44ba105ba6dc9615e
Jacob Milligan <jacobpmilligan@gmail.com>
2018-04-10 13:35:18 +1000

    docs: add doc comments to preprocessor

--- shadecc/preprocessor.py
+++ shadecc/preprocessor.py
@@ -5,7 +5,8 @@ PRAGMA_LINE = '#pragma shadecc_'
 
 
 class InVar(object):
-    def __init__(self, var_type, num_elems, line):
+    def __init__(self, raw_name, var_type, num_elems, line):
+        self.raw_name = raw_name
         self.var_type = var_type
         self.num_elements = num_elems
         self.line = line
@@ -31,6 +32,17 @@ class PragmaProcessor(object):
 
 class StageProcessor(PragmaProcessor):
     def process(self, line, shader, line_num):
+        """
+        Preprocess `#pragma shadecc_stage`
+        :param line:
+        :type line:
+        :param shader:
+        :type shader:
+        :param line_num:
+        :type line_num:
+        :return:
+        :rtype:
+        """
         shader.stage = line
         return ''
 
@@ -40,29 +52,60 @@ class NameProcessor(PragmaProcessor):
         self.__regex = re.compile(r'"([^"]*)"')
 
     def process(self, line, shader, line_num):
+        """
+        Preprocess `#pragma shadecc_name`
+        :param line:
+        :type line:
+        :param shader:
+        :type shader:
+        :param line_num:
+        :type line_num:
+        :return:
+        :rtype:
+        """
         shader.name = self.__regex.findall(line)[0]
         return ''
 
 
 class InstancedProcessor(PragmaProcessor):
     ALLOWED_TYPES = ['vec1', 'vec2', 'vec3', 'vec4', 'mat4']
+    INSTANCE_PREFIX = 'SHADECC_INSTANCED_'
 
     def __init__(self):
         self.loc_re = re.compile(r'layout \((location[\s]*=[\s]*([0-9]*))\)')
 
     def process(self, line, shader, line_num):
+        """
+        Preprocesses a `#pragma shadecc_instanced` declaration, unrolling instanced variables as
+        required
+        :param line:
+        :type line:
+        :param shader:
+        :type shader:
+        :param line_num:
+        :type line_num:
+        :return:
+        :rtype:
+        """
         parts = line.split()
         for t in self.ALLOWED_TYPES:
-            index = parts.index(t) if t in parts else -1
-            if index > -1 and t.startswith('mat'):
-                num_elems = parts[index].replace('mat', '')
-                name = parts[index + 1]
-                replacement = line.replace('mat', 'in vec')
-                shader.instanced_vars[name] = InVar(t, int(num_elems), line_num)
-
+            type_index = parts.index(t) if t in parts else -1
+            # Get index of the type and, if matrix, replace with vec type
+            if type_index > -1 and t.startswith('mat'):
+                mat_elem_count = parts[type_index].replace('mat', '')
+                raw_name = parts[type_index + 1]
+                inst_name = InstancedProcessor.INSTANCE_PREFIX + raw_name
+                # Replace type name, and identifier (prefix with `SHADECC_INSTANCED_`)
+                replacement = line.replace('mat', 'in vec').replace(raw_name, inst_name)
+                # Add to dict for find/replace later in the code body
+                shader.instanced_vars[inst_name] = InVar(raw_name, t, int(mat_elem_count), line_num)
                 parts.clear()
-                for elem in range(int(num_elems)):
+
+                # Unroll the matrix into vec4 variables - for mat4 result will be
+                # 4 vec4 elements named e.g. SHADECC_INSTANCED_model_0, SHADECC_INSTANCED_model_1...
+                for elem in range(int(mat_elem_count)):
                     parts.append('{0}_{1}'.format(replacement, elem))
+                    # gets `layout (location = n)` and replaces with n + 1
                     re_result = self.loc_re.search(parts[elem])
                     loc_str = re_result.group(1)
                     base_loc = re_result.group(2)
@@ -80,6 +123,13 @@ class Preprocessor(object):
     }
 
     def process(self, file_contents):
+        """
+        Preprocesses a shader file and replaces all shadecc #pragma calls with standard GLSL
+        :param file_contents:
+        :type file_contents:
+        :return:
+        :rtype:
+        """
         shader = Shader()
         lines = file_contents.split('\n')
         lines = [self.__process_pragma(l, shader, i) for i, l in enumerate(lines)]
@@ -90,12 +140,50 @@ class Preprocessor(object):
         return shader
 
     def __replace_instanced_var(self, name, var, lines):
+        """
+        Replaces all usages of an instanced variable with its modified name and format. For example
+        a mat4 instanced variable used like so:
+
+        ```
+        gl_Position = viewproj * model * position;
+        ```
+
+        Will be replaced with:
+
+        ```
+        gl_Position = viewproj * mat4(SHADECC_INSTANCED_model_0, SHADECC_INSTANCED_model_1, ...)
+                      * position;
+        ```
+
+        :param name: The reformatted name to replace, i.e. SHADECC_INSTANCED_model
+        :type name: str
+        :param var: The in variable object
+        :type var: InVar
+        :param lines: List of the shaders source lines
+        :type lines: list
+        :return: The source lines modified to replace the variable
+        :rtype: list
+        """
         if var.var_type.startswith('mat'):
             args = ['{0}_{1}'.format(name, i) for i in range(var.num_elements)]
             replacement = '{0}({1})'.format(var.var_type, ', '.join(args))
-            return [l.replace(name, replacement) if 'layout' not in l else l for l in lines]
+            result = [l.replace(var.raw_name, replacement) if 'layout' not in l
+                      else l
+                      for l in lines]
+            return result
 
     def __process_pragma(self, line, shader, line_num):
+        """
+        Processes a single shadecc #pragma declaration
+        :param line:
+        :type line:
+        :param shader:
+        :type shader:
+        :param line_num:
+        :type line_num:
+        :return:
+        :rtype:
+        """
         if not line.startswith(PRAGMA_LINE):
             return line
         line_parts = line.split()
